b80e668da5846806fcc337fe125648a073d92ffc
Galvandi <andreas_schoch@outlook.com>
2019-09-22 20:16:05 +0200

    Updates

---
+++ precache-manifest.6de4b1a5e8dc25863968199114cb79c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d16271212c0e0353aab21779c37f9e1",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "7a002ce08c8e9b65f811",
+    "url": "/react-todo-app/static/css/main.9e1436f5.chunk.css"
+  },
+  {
+    "revision": "506d639e179ad5f4b78f",
+    "url": "/react-todo-app/static/js/2.16c2c9c2.chunk.js"
+  },
+  {
+    "revision": "7a002ce08c8e9b65f811",
+    "url": "/react-todo-app/static/js/main.d04d114a.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
